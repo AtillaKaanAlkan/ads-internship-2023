f3035f24bbdfa9453f69d9df9b5734d2a84e103c
Atilla Alkan <alkan@compta78.limsi.fr>
2023-07-25 17:22:26 -0400

    Script to convert BRAT standoff format into JSONLINES format for Fast-Coref usage

---
+++ coref_resolution/standoff2jsonlines.py
@@ -0,0 +1,124 @@
+import json
+import glob
+import gzip
+import argparse
+
+""" 
+
+This script is useful if you want to use Fast-Coref for coreference resolution.
+This script converts your corpus (in BRAT annotations format (.ann files)) into JSONLINES format for Fast-Coref.
+
+"""
+
+parser = argparse.ArgumentParser(description = 'This script is for converting BRAT annotations (.ann files) in .jsonlines format to use Fast-Coref model')
+parser.add_argument('--corpus_dir', required = True, type = str)
+args = parser.parse_args()
+path_to_folder = args.corpus_dir
+
+
+def dicts_to_jsonl(data_list: list, filename: str, compress: bool = True) -> None:
+    """
+    Method saves list of dicts into jsonlines file.
+
+    :param data: (list) list of dicts to be stored,
+    :param filename: (str) path to the output file. If suffix .jsonlines is not given then methods appends
+        .jsonlines suffix into the file.
+    :param compress: (bool) should file be compressed into a gzip archive?
+    """
+
+    sjsonl = '.jsonlines'
+    sgz = '.gz'
+
+    # Check filename
+
+    if not filename.endswith(sjsonl):
+        filename = filename + sjsonl
+
+    # Save data
+    
+    if compress:
+        filename = filename + sgz
+        with gzip.open(filename, 'w') as compressed:
+            for ddict in data_list:
+                jout = json.dumps(ddict) + '\n'
+                jout = jout.encode('utf-8')
+                compressed.write(jout)
+    else:
+        with open(filename, 'w') as out:
+            for ddict in data_list:
+                jout = json.dumps(ddict) + '\n'
+                out.write(jout)
+
+
+def standoff2jsonlines(path_to_folder, output_filename):
+
+    ann_files = glob.glob(path_to_folder + '*.ann')
+
+    list_of_dict = []
+
+    for file in ann_files:
+
+        filename = file.split(path_to_folder)[1].split('.ann')[0]
+
+        with open(f'{path_to_folder}/{filename}.ann', 'r') as ann:
+            ann = ann.read()
+
+        with open(f'{path_to_folder}/{filename}.txt', 'r') as text:
+            text = text.read()
+
+        d = {}
+        d['text'] = text
+
+        unique_arg2 = []
+        relations_info = []
+
+        for row in ann.split('\n'):
+                row = row.split('\t')
+                if row != ['']:
+                    if row[1].startswith('coreferring_to '):
+                        link = row[1].split('coreferring_to ')
+                        link = link[1]
+                        relations_info.append(link)
+                        arg1 = link.split('Arg1:')[1].split()[0]
+                        arg2 = link.split('Arg2:')[1].split()[0]
+                        if arg2 not in unique_arg2:
+                            unique_arg2.append(arg2)
+
+        clusters = []
+        clusters_strings = []
+
+        for entity_T in unique_arg2:
+            
+            mentions = []
+            spans = []
+            
+            for r in relations_info:
+                if r.endswith(f'Arg2:{entity_T}'):
+                    arg1 = r.split()[0].split('Arg1:')[1]
+                    for line in ann.split('\n'):
+                        if line.startswith(f'{arg1}\t'):
+                            m = line.split('\t')[-1] # get the mention
+                            span_start, span_end = line.split('\t')[-2].split()[-2], line.split('\t')[-2].split()[-1] # get the spans (starting and ending) of the mention
+                            spans.append([span_start, span_end])
+                            mentions.append(m)
+            
+            for line in ann.split('\n'):
+                if line.startswith(f'{entity_T}\t'):
+                    mentions.append(line.split('\t')[-1])
+                    span_start, span_end = line.split('\t')[-2].split()[-2], line.split('\t')[-2].split()[-1] # get the spans (starting and ending) of the mention
+                    spans.append([span_start, span_end])
+
+
+            clusters_strings.append(mentions)
+            clusters.append(spans)
+        
+        d['clusters'] = clusters
+        d['clusters_strings'] = clusters_strings
+
+        list_of_dict.append(d)
+
+
+    dicts_to_jsonl(list_of_dict, f'{output_filename}', False)
+
+
+standoff2jsonlines(path_to_folder, 'TDAC_corpus')
