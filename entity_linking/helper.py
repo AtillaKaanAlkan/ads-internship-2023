e5dcf8ce4c3ecf953ed47739ff2b7cd783b8d53a
Atilla Alkan <alkan@compta78.limsi.fr>
2023-08-04 11:29:16 -0400

    some helper functions

---
+++ entity_linking/helper.py
@@ -0,0 +1,20 @@
+import seqeval
+from seqeval.metrics.sequence_labeling import *
+
+def extract_only_celestial_object_from_ner_module_output(tokens, tags):
+    
+    Cob_only = [elem for elem in get_entities(tags) if elem[0] == 'CelestialObject']
+    
+    sources_names = []
+
+    for cob in Cob_only:
+        sources_names.append(" ".join(w for w in tokens[cob[1]: cob[-1]]))
+    
+    return Cob_only
+
+def parse_input_source_name(list_of_source_name):
+
+    parsed_source_name = list_of_source_name[1:-1].split(',')
+
+    return parsed_source_name
+
